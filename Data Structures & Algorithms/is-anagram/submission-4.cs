206fe23126f78be24646c518847597e4b1e2c0dc
Rooo1o <32430285+Rooo1o@users.noreply.github.com>
2026-04-06 15:27:26 +0200

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if((s.Length != t.Length) || (s.Length == 0)) return false;
+        List<char> helperList = new List<char>();
+        foreach(char c in s) {
+            helperList.Add(c);
+        }
+
+        foreach(char c in t) {
+            if(helperList.Contains(c)){
+                helperList.Remove(c);
+            } else return false;
+        }
+        return helperList.Count == 0;
+    }
+}
+
+/*public class Solution2 {
+    public bool IsAnagram(string s, string t) {
+      //  if(s.Length != t.Length) || (s.Length == 0 || t.Length == 0) return false;
+        return false;
+    }
+}*/
