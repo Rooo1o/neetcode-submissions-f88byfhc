103f4b0d659b1a66513200067385b881656f2eaa
Rooo1o <32430285+Rooo1o@users.noreply.github.com>
2026-04-12 14:53:52 +0200

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public bool IsValid(string s) {
+        if(s.Length % 2 != 0) return false;
+        Dictionary<char, char> dClosers = new Dictionary<char, char>();
+        var openers = new HashSet<char> {'(', '{', '['};
+        var closers = new HashSet<char> {')', '}', ']'};
+        dClosers.Add(')', '(');
+        dClosers.Add('}', '{');
+        dClosers.Add(']', '[');
+        var stack = new Stack<char>();
+        foreach (char c in s) {
+            if (openers.Contains(c)) {
+                stack.Push(c);
+                continue; 
+            }
+            if(stack.Count == 0) {
+                return false;
+            }
+            if(dClosers.TryGetValue(c, out char value)){
+                if(stack.Pop() != value) return false;
+            } else {
+                throw new ArgumentException("Something went very wrong here");
+            }
+        }
+        return stack.Count == 0;
+    }
+}
