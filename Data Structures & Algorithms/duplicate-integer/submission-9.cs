43eb79107efd83e99ff0896d6239e6e973038de5
Rooo1o <32430285+Rooo1o@users.noreply.github.com>
2026-05-04 16:04:21 +0200

    Add: duplicate-integer - submission-9

---
+++ Data Structures & Algorithms/duplicate-integer/submission-9.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        HashSet<int> ints = new HashSet<int>();
+        for(int i = 0; i < nums.Length; i++) {
+            if(!ints.Add(nums[i])) return true;
+        }
+        return false;
+    }
+}
