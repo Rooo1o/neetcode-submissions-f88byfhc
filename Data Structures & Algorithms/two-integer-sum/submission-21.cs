da4bfa8610972e7a09d9f6f244cd29aa02c3b078
Rooo1o <32430285+Rooo1o@users.noreply.github.com>
2026-05-27 18:13:14 +0200

    Add: two-integer-sum - submission-21

---
+++ Data Structures & Algorithms/two-integer-sum/submission-21.cs
@@ -0,0 +1,49 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> diffs = new Dictionary<int, int>();
+        for(int i = 0; i < nums.Length; i++) {
+            int diff = target - nums[i];
+            if (diffs.ContainsKey(diff)) return new int[] {diffs[diff], i};
+            diffs[nums[i]] = i;
+        } return null;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> diffs = new Dictionary<int, int>();
+        for(int i = 0; i < nums.Length; i++) {
+            int diff = target - nums[i];
+            if(diffs.ContainsKey(diff)) {
+                return new int[] { diffs[diff], i };
+            }
+            diffs[nums[i]] = i;
+        }
+        return null;
+    }*/
+}
